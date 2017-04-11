3dd266e15e7c875f9fd92ef4f8e254d226a86bf7
campbell88 <shatharcampbell@gmail.com>
2017-04-11 10:37:30 -0400

    initial commit

---
+++ Lab 1 - Area and Perimeter of GC Rooms/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1___Area_and_Perimeter_of_GC_Rooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool askToContinue = false; // do while to ask user to continue
+            do
+            {
+                Console.WriteLine("Enter Length of Room:");
+                double length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Width of Room:");
+                double width = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Height of Room:");
+                double height = double.Parse(Console.ReadLine());
+
+                double area = length * width;
+                double perimeter = 2 * (length + width);
+                double volume = length * width * height;
+                
+
+                Console.WriteLine("Area of room is {0} and Perimeter of room is {1} and Volume is {2}", area, perimeter, volume);
+                // where to add '\n' to add a line up above
+                Console.WriteLine("Type YES to continue"); //need help with my do loop
+
+                if (Console.ReadLine().ToUpper() == "YES")             
+                    askToContinue = true;
+                
+            } while (askToContinue);
+
+
+
+
+
+
+
+        }
+    }
+}
